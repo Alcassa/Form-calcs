9dfb3196e35fb7e9a87a39588cb1cebe67ec996b
Alcassa <galcassa49@gmail.com>
2023-09-22 15:01:39 -0300

    Arrumando a calculadora e corrigindo erros

--- SolucaoCalcs/login/FormLogin.Designer.cs
+++ SolucaoCalcs/login/FormLogin.Designer.cs
@@ -74,8 +74,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
+            this.button1.Text = "Cancel";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnLogin
             // 
--- SolucaoCalcs/login/FormLogin.cs
+++ SolucaoCalcs/login/FormLogin.cs
@@ -36,5 +36,11 @@ namespace SolucaoCalcs.login
             }
             catch (Exception ex) { }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtUser.Text = "";
+            txtPassword.Text = "";
+        }
     }
 }
